077645ade3f27109ecf5bcf0bd7333cc36cf1d72
Apiwith Potisuk <po.apiwith@gmail.com>
2022-10-18 02:38:46 +0700

    Sub window redesign
    Sub window + mini pane redesign

--- dist/main.js
+++ dist/main.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$("#bt-profile, #bt-back").click(function() {
 		$("#main-container").toggleClass('detail-open');
 	});
+
+	$("#bt-color, #bt-mini-exit").click(function() {
+		$(".mini-pane-stage").toggleClass('mps-open');
+	});
 });
